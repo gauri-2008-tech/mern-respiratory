0a667e356cdf403f1c6f1a2666a060332458928b
gauri-2008-tech <gauri.th2008@gmail.com>
2026-06-23 11:47:16 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,11 @@
+let name = "Gauri Thengari";
+let age = 18; // or your age
+let branch = "Computer Engineering";
+let favouriteSubject = "JavaScript"; // or your fav subject
+let github = "gauri-2008-tech";
+
+console.log("Student Name: " + name);
+console.log("Age: " + age);
+console.log("Branch: " + branch);
+console.log("Favourite Subject: " + favouriteSubject);
+console.log("GitHub: " + github);
